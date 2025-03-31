c8165b403268622db66ee83dedea4c43adca2849
passwordd009 <elijah.hawes47@gmail.com>
2025-03-31 17:00:12 -0400

    removed radiz depencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-      <App />
-  
+      <App /> 
   </React.StrictMode>,
 )
