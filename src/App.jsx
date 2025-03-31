46d39a1d05be17e02b8710cbb7790ed738401532
passwordd009 <elijah.hawes47@gmail.com>
2025-03-30 22:21:24 -0400

    finished

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import HeartAnimation from '../component/HeartAnimation'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <HeartAnimation></HeartAnimation>
+    </>
+  )
+}
+
+export default App
